f356aac60bcfefc3d768b050fbd548eaf44ca851
Gio d'Amelio <giodamelio@gmail.com>
2018-01-28 18:18:17 -0800

    Only use the minimist for the server command

--- tiddlywiki.js
+++ tiddlywiki.js
@@ -22,19 +22,22 @@ const argv = minimist(process.argv.slice(2), {
 });
 
 // Pass the command line arguments to the boot kernel
-$tw.boot.argv = [
-  argv._[0], // Direcory
-  '--' + argv._[1], // Command
-  argv.port, // Port
-  argv['root-tiddler'], // Root tiddler
-  argv['render-type'], // Render mime type
-  argv['serve-type'], // Serve mime type
-  argv.username, // Username
-  argv.password, // Passport
-  argv.host, // Host
-  argv['path-prefix'] // Path pretix
-];
-// $tw.boot.argv = Array.prototype.slice.call(process.argv,2);
+if (argv._[1] === 'server') {
+  $tw.boot.argv = [
+    argv._[0], // Direcory
+    '--' + argv._[1], // Command
+    argv.port, // Port
+    argv['root-tiddler'], // Root tiddler
+    argv['render-type'], // Render mime type
+    argv['serve-type'], // Serve mime type
+    argv.username, // Username
+    argv.password, // Passport
+    argv.host, // Host
+    argv['path-prefix'] // Path pretix
+  ];
+} else {
+  $tw.boot.argv = Array.prototype.slice.call(process.argv,2);
+}
 
 // Boot the TW5 app
 $tw.boot.boot();
